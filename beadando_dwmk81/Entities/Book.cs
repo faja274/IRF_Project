03b26d2e5b062a2228f3a21da8c87b6abcf2d750
Fajkusz András <andras.fajkusz@stud.uni-corvinus.hu>
2020-12-07 17:12:43 +0100

    book class letrehozasa

---
+++ beadando_dwmk81/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beadando_dwmk81.Entities
+{
+    public class Book
+    {
+        public Guid ID { get; set; }
+        public string Topic { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+        public DateTime SellDate { get; set; }
+    }
+}
