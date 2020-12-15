6166875d1a7774bfd65583a61828a6a7140edd09
Fajkusz András <andras.fajkusz@stud.uni-corvinus.hu>
2020-12-15 15:50:00 +0100

    csv betoltesenek kijavitasa

--- beadando_dwmk81/Form1.cs
+++ beadando_dwmk81/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
